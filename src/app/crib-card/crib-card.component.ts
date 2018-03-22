b416ee41ed82ad84565becf6ab6a18a4178e4f82
Mohit Sharma <mohitroxx04@gmail.com>
2018-03-23 03:37:17 +0530

    Add files via upload

---
+++ src/app/crib-card/crib-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Crib } from './../crib';
+
+@Component({
+  selector: 'app-crib-card',
+  templateUrl: './crib-card.component.html',
+  styleUrls: ['./crib-card.component.css']
+})
+export class CribCardComponent implements OnInit {
+
+  @Input('crib') crib:Crib;
+  constructor() { }
+
+  ngOnInit() {
+    let bedroom=this.crib.bedroom;
+    //let kitchen=this.crib.kitchen;
+  }
+
+}
